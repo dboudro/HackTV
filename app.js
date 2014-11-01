6151187963b39c3fcbf65c652a5817291d6369df
Max Lever <maxdavidlever@gmail.com>
2014-11-01 18:50:01 -0400

    add routes, views

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+config = require('./lib/config');
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -7,8 +8,33 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var login = require('./routes/login');
+var board = require('./routes/board');
 
-var app = express();
+// var app = express();
+var app = express.createServer(),
+    http = require('http'),
+    https = require('https'),
+    connect = require('connect'),
+    redis = require('connect-redis')(express),
+    everyauth = require('everyauth'),
+    sesh = new redis();
+
+everyauth.github
+  .appId(config.gh_clientId)
+  .appSecret(config.gh_secret)
+  .findOrCreateUser( function (session, accessToken, accessTokenExtra, githubUserMetadata) {
+    session.oauth = accessToken;
+    return session.uid = githubUserMetadata.login;
+  })
+  .redirectPath('/');
+ everyauth.everymodule.handleLogout( function (req, res) {
+  req.logout(); 
+  req.session.uid = null;
+  res.writeHead(303, { 'Location': this.logoutRedirectPath() });
+  res.end();
+});
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,9 +47,14 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
 app.use('/users', users);
+app.use('/login', login);
+app.use('/board', board);
+
+app.register('.html', require('jqtpl').express);
+app.use(express.session({store: sesh, secret: config.redis_secret}));
+app.use(everyauth.middleware()); // pretend you didn't see this yet
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/login.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+// /* GET home page. */
+// router.get('/', function(req, res) {
+//   res.render('login');
+// });
+
+router.get('/',function(req,res) {
+  if (req.session && req.session.uid) {
+      return res.redirect('/board');
+  }
+  res.render('login');
+});
+
+module.exports = router;
