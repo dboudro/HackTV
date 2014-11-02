ff6b0aa62f8aff81ee941535892b072adeb2c422
Dylan Boudro <Dboody@Dylans-MacBook-Pro.local>
2014-11-02 10:18:01 -0500

    layout structure complete

--- public/javascripts/getVideos.js
+++ public/javascripts/getVideos.js
@@ -1,6 +1,6 @@
  var idPulled = "4193865";
 function x(idx) {
- return "<object bgcolor='#000000' data='http://www.twitch.tv/widgets/archive_embed_player.swf' height='378' id='clip_embed_player_flash' type='application/x-shockwave-flash' width='620'><param name='movie' value='http://www.twitch.tv/widgets/archive_embed_player.swf' /><param name='allowScriptAccess' value='always' /><param name='allowNetworking' value='all' /><param name='allowFullScreen' value='true' /><param name='flashvars' value='channel=ludendi&start_volume=25&auto_play=false&chapter_id=" + idx + "'/></object>";
+ return "<object bgcolor='#000000' data='http://www.twitch.tv/widgets/archive_embed_player.swf' height='476' id='clip_embed_player_flash' type='application/x-shockwave-flash' width='782'><param name='movie' value='http://www.twitch.tv/widgets/archive_embed_player.swf' /><param name='allowScriptAccess' value='always' /><param name='allowNetworking' value='all' /><param name='allowFullScreen' value='true' /><param name='flashvars' value='channel=ludendi&start_volume=25&auto_play=false&chapter_id=" + idx + "'/></object>";
 }
 
 
